d66123d55496e5b99a0c5e4611e7d8c907d6d595
cuito <cuitonaude@gmail.com>
2018-11-21 00:18:28 +0200

    bip bop

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -64,7 +64,7 @@ function goToGoogleForm() {
 }
 
 function goToMap() {
-  window.location.assign("https://goo.gl/maps/vX3N6xiaVLR2");
+  window.location.assign("https://goo.gl/maps/pdQQuBTNz332");
 }
 
 function sendEmail() {
@@ -90,4 +90,78 @@ function chooseKakLekker() {
   document.getElementById("lekker").style.backgroundColor = "white";
   document.getElementById("kak").style.backgroundColor = "white";
   document.getElementById("kaklekker").style.backgroundColor = "black";
+}
+
+function goToArtist(number) {
+	switch (number) {
+		case 1:
+			window.open("https://soundcloud.com/jnn-kpn", "_blank");
+			break;
+		case 2:
+			window.open("https://soundcloud.com/1991-global/gourmet-delicious", "_blank");
+			break;
+		case 3:
+			window.open("https://soundcloud.com/wetdreamsrecordings/sets/rose-bonica-dont-let-it-get-to", "_blank");
+			break;
+		case 4:
+			window.open("https://soundcloud.com/albanylore/rest-easy-genie", "_blank");
+			break;
+		case 5:
+			window.open("https://www.mixcloud.com/Redbullstudioct/red-bull-studio-guest-dj-mix-006-lady-venom/", "_blank");
+			break;
+		case 6:
+			window.open("https://soundcloud.com/intrinsic-audio-sessions/schumi-explorations-ep-snippet-intrinsic-audio-01", "_blank");
+			break;
+		case 7:
+			window.open("https://soundcloud.com/garyreubenmorris/gary-morris-live-set-welcome-park-soiree-december-2017", "_blank");;
+			break;
+		case 8:
+			window.open("https://soundcloud.com/smalltownbeat/kevin-murning-smalltown-beat-2018-live-recording", "_blank");
+			break;
+		case 9:
+			window.open("https://soundcloud.com/hear_capetown/hearcast_005-joymode", "_blank");
+			break;
+		case 10:
+			window.open("https://soundcloud.com/sukummah/20180324a", "_blank");
+			break;
+		case 11:
+			window.open("https://www.instagram.com/qqueezyy/?hl=en", "_blank");
+			break;
+		case 12:
+			window.open("https://soundcloud.com/jonlaura/rose-gold-rose", "_blank");
+			break;
+		case 13:
+			window.open("https://soundcloud.com/decodedmagazine/exclusive-premiere-thor-rixon-old-strandfontein-road-get-physical-music", "_blank");
+			break;
+		case 14:
+			window.open("https://soundcloud.com/1991-global/maxime-alexander-salon-rouge", "_blank");
+			break;
+		case 15:
+			window.open("https://www.mixcloud.com/TheOtherRadio/the-other-djs-ctemf-mix/", "_blank");
+			break;
+		case 16:
+			window.open("https://www.mixcloud.com/TheOtherRadio/the-other-radio-10-ft-not-sorry-club/", "_blank");
+			break;
+		case 17:
+			window.open("https://www.mixcloud.com/jarred-parenzee/may-2018-studio-mix/", "_blank");;
+			break;
+		case 18:
+			window.open("https://soundcloud.com/fred-buddah/sets/the-hip-scientist-ep", "_blank");
+			break;
+		case 19:
+			window.open("https://soundcloud.com/spoorpeople/trail-mix-11-t-siza-super-black-tapes-takeover", "_blank");
+			break;
+		case 20:
+			window.open("https://www.instagram.com/sam_heuermann/?hl=en", "_blank");
+			break;
+		case 21:
+			window.open("https://www.instagram.com/buu.jinn/", "_blank");
+			break;
+		case 22:
+			window.open("https://www.mixcloud.com/TheOtherRadio/the-other-radio-12-ft-slow-motion-city-luis93/", "_blank");
+			break;
+		case 23:
+			window.open("https://soundcloud.com/king-recording/cuito-4", "_blank");
+			break;
+	}
 }
